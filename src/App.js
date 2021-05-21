2889ccae474a308338087613827f94472130f683
Bhatnagar <pb185199@ncr.com>
2021-05-21 18:10:38 +0530

    redirect-link

--- src/App.js
+++ src/App.js
@@ -5,21 +5,20 @@ import EducationSection from './js/EducationSection';
 import ProjectWork from './js/ProjectWork';
 import Experience from './js/Experience';
 import Contact from './js/Contact';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Email from './js/Email';
 export default function App() {
     return (
-        <BrowserRouter>
-            <div>
-                <Switch>
-                    <Route path="/" component={HomeSection} exact={true} />
-                    <Route path="/abt" component={AboutSection} />
-                    <Route path="/edu" component={EducationSection} />
-                    <Route path="/exp" component={Experience} />
-                    <Route path="/pro" component={ProjectWork} />
-                    <Route path="/con" component={Contact} />
-                </Switch>
-            </div>
-        </BrowserRouter>
+        <div>
+            <Switch>
+                <Route path="/" component={HomeSection} exact={true} />
+                <Route path="/abt" component={AboutSection} />
+                <Route path="/edu" component={EducationSection} />
+                <Route path="/exp" component={Experience} />
+                <Route path="/pro" component={ProjectWork} />
+                <Route path="/con" component={Contact} />
+                <Redirect to="/" />
+            </Switch>
+        </div>
     );
 }
